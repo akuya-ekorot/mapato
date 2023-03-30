1740943137a69840db09a853fd24e6121b310f9c
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-30 12:16:15 +0300

    hero height

--- src/app/page.js
+++ src/app/page.js
@@ -156,7 +156,7 @@ const AboutUs = () => {
 
 const Hero = () => {
   return (
-    <section id="about-us" className="lg:py-20 md:py-16 py-14 lg:px-20 px-10 w-full flex flex-col items-center justify-center">
+    <section id="about-us" className="h-[40vh] md:h-[50vh] lg:h-[60vh] lg:py-20 md:py-16 py-14 lg:px-20 px-10 w-full flex flex-col items-center justify-center">
       <div className="w-full lg:max-w-7xl max-w-5xl">
         <h1 className={`lg:text-7xl md:text-5xl md:text-left text-center text-4xl lg:max-w-6xl max-w-xl ${display.className}`}>Get personalized and creative marketing for your business.</h1>
       </div>
